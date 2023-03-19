ecfb34b99492abd02de2f66248c33d53b1a1c898
Ilim001 <ilim.iskenderov@gmail.com>
2023-03-19 15:55:29 +0600

    init project

---
+++ script.js
@@ -0,0 +1,194 @@
+
+
+// English version
+// const body = document.body
+// const h3 = document.createElement('h3')
+// h3.innerHTML ='Name:'
+// body.prepend(h3)
+
+
+// const input = document.createElement('input')
+// input.setAttribute("placeholder","Name" )
+// input.setAttribute("class","input" )
+// h3.after(input)
+
+// input.style.background = '#d7d7d7'
+
+// const h3b = document.createElement('h3')
+// h3b.innerHTML ='Number:'
+// input.after(h3b)
+
+
+// const input2 = document.createElement('input')
+// input2.setAttribute("placeholder","Number" )
+// input2.setAttribute("class","input" )
+// h3b.after(input2)
+
+
+
+// const h3c = document.createElement('h3')
+// h3c.innerHTML ='Email:'
+// input2.after(h3c)
+
+
+// const input3 = document.createElement('input')
+// input2.setAttribute("placeholder","Email")
+// input2.setAttribute("class",)
+
+
+// h3c.after(input3)
+
+
+
+// const h3d = document.createElement('h3')
+// h3d.innerHTML ='Sum:'
+// input3.after(h3d)
+
+
+// const input4 = document.createElement('input')
+// input4.setAttribute("placeholder","Sum" )
+// h3d.after(input4)
+
+
+// const btn = document.createElement('button')
+// btn.innerHTML ='Go to the payment'
+// btn.setAttribute("class","btn")
+// input4.after(btn)
+
+// btn.style.display = "block"
+// btn.style.marginTop = "20px"
+// btn.style.width ="200px"
+// btn.style.height ="50px"
+// btn.style.background = "#ff5545"
+// btn.style.border = "none"
+// btn.style.borderRadius = "8px"
+// btn.style.color = "white"
+
+// input.style.background = "#d7d7d7"
+// input.style.width = "400px"
+// input.style.height = "50px"
+// input.style.border = "none"
+// input.style.borderRadius = "8px"
+
+
+// input2.style.background = "#d7d7d7"
+// input2.style.width = "400px"
+// input2.style.height = "50px"
+// input2.style.border = "none"
+// input2.style.borderRadius = "8px"
+
+
+
+// input3.style.background = "#d7d7d7"
+// input3.style.width = "400px"
+// input3.style.height = "50px"
+// input3.style.border = "none"
+// input3.style.borderRadius = "8px"
+
+
+// input4.style.background = "#d7d7d7"
+// input4.style.width = "400px"
+// input4.style.height = "50px"
+// input4.style.border = "none"
+// input4.style.borderRadius = "8px"
+
+
+
+
+
+
+// Russian version
+// const body = document.body
+// const h3 = document.createElement('h3')
+// h3.innerHTML = 'Фамилия Имя:'
+// body.prepend(h3)
+
+// const input = document.createElement('input')
+// input.setAttribute("placeholder","Имя")
+// input.setAttribute("class","input")
+// h3.after(input)
+
+
+// const hh5 = document.createElement('h3')
+// hh5.innerHTML = 'Номер:'
+// body.append(hh5)
+
+// const input2 = document.createElement('input')
+// input2.setAttribute("placeholder","")
+// input2.setAttribute("class","input")
+// hh5.after(input2)
+
+
+// const hh6 = document.createElement('h3')
+// hh6.innerHTML = 'Введите номер заказа:'
+// body.append(hh6)
+
+// const input3 = document.createElement('input')
+// input3.setAttribute("placeholder","Оставьте сообщение здесь")
+// input3.setAttribute("class","input")
+// hh6.after(input3)
+
+
+// const hh7 = document.createElement('h3')
+// hh7.innerHTML = 'Электронный адрес:'
+// body.append(hh7)
+
+// const input4 = document.createElement('input')
+// input4.setAttribute("placeholder","hellooo@example.com")
+// input4.setAttribute("class","input")
+// hh7.after(input4)
+
+// const hh8 = document.createElement('h3')
+// hh8.innerHTML = 'Введите сумму оплаты:'
+// body.append(hh8)
+
+// const input5 = document.createElement('input')
+// input5.setAttribute("placeholder","")
+// input5.setAttribute("class","input")
+// hh8.after(input5)
+
+// const btn = document.createElement('button')
+// btn.innerHTML = 'Перейти к оплате'
+// btn.setAttribute("class","btn")
+// input5.after(btn)
+
+
+// btn.style.display = "block"
+// btn.style.marginTop = "20px"
+// btn.style.width ="200px"
+// btn.style.height ="50px"
+// btn.style.background = "#ff5545"
+// btn.style.border = "none"
+// btn.style.borderRadius = "8px"
+// btn.style.color = "white"
+
+// input.style.background = "#d7d7d7"
+// input.style.width = "400px"
+// input.style.height = "50px"
+// input.style.border = "none"
+// input.style.borderRadius = "8px"
+
+// input2.style.background = "#d7d7d7"
+// input2.style.width = "400px"
+// input2.style.height = "50px"
+// input2.style.border = "none"
+// input2.style.borderRadius = "8px"
+
+// input3.style.background = "#d7d7d7"
+// input3.style.width = "400px"
+// input3.style.height = "50px"
+// input3.style.border = "none"
+// input3.style.borderRadius = "8px"
+
+
+// input4.style.background = "#d7d7d7"
+// input4.style.width = "400px"
+// input4.style.height = "50px"
+// input4.style.border = "none"
+// input4.style.borderRadius = "8px"
+
+// input5.style.background = "#d7d7d7"
+// input5.style.width = "400px"
+// input5.style.height = "50px"
+// input5.style.border = "none"
+// input5.style.borderRadius = "8px"
